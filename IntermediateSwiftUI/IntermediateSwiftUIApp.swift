9100d956c1512c6b36a4ca463451535309a1ccb6
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2023-03-22 12:04:24 -0500

    Initial Commit

---
+++ IntermediateSwiftUI/IntermediateSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntermediateSwiftUIApp.swift
+//  IntermediateSwiftUI
+//
+//  Created by Justin Hold on 3/22/23.
+//
+
+import SwiftUI
+
+@main
+struct IntermediateSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
